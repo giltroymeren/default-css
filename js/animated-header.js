cbf5ec7322d4d87d2e52a2d8d507296b5ecb207e
troymeren <troymeren@gmail.com>
2013-06-08 23:34:20 +0800

    Set base animated header style and behavior

---
+++ js/animated-header.js
@@ -0,0 +1,39 @@
+/*
+ *	animated-header.js
+ *	Manouella Ilic
+ *	http://tympanus.net/codrops/2013/06/06/on-scroll-animated-header/
+ */
+var animatedHeader = (function() {
+ 
+    var docElem = document.documentElement,
+        header = document.querySelector( '#header-animated' ),
+        didScroll = false,
+        changeHeaderOn = 300;
+ 
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
+ 
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'header-shrink' );
+        }
+        else {
+            classie.remove( header, 'header-shrink' );
+        }
+        didScroll = false;
+    }
+ 
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+ 
+    init();
+ 
+})();
